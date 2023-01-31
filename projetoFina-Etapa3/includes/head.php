9ad95a67b23cd1511cb15155a388b42d2443b376
gabrielssouza23 <94071390+gabrielssouza23@users.noreply.github.com>
2023-01-31 13:00:35 -0300

    Delete projetoFina-Etapa3 directory

--- projetoFina-Etapa3/includes/head.php
+++
@@ -1,7 +0,0 @@
-
-    <meta charset="UTF-8">
-    <title>Tintendo Gallery</title>
-    <link rel="shortcut icon" href="./img/horizontal_on_white_by_logaster-removebg-preview_1__1_.ico" type="image/x-icon">
-    <link rel="stylesheet" href="./assets/style.css">
-    <script src="./assets/home.js" async></script>
-    <script src="./assets/script.js" async></script>
